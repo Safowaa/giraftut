41a41edcf4480fa4c52bbb408c1a2c455cbfd009
Safowaa <safowaabenedicta@gmail.com>
2022-11-18 15:33:56 +0000

    initial

---
+++ look.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+$phrase = "Giraffe Academy";
+
+// turns strings to lower and upper cases respectively
+// echo strtolower($phrase) ;
+
+// echo strtoupper("dog");
+
+// gives the lenght of a string
+// echo $phrase [11];
+
+
+// how to use the various operands in php
+// echo 10 % 3;
+// echo ( 4 + 5) * 10;
+// $num = 10;
+// $num - -; 
+// $num += 25;
+// echo $num;
+
+// returns a positive value
+// echo abs(-100); 
+
+// raises 2 to the power of 4
+// echo pow(2, 4);
+
+// finds the square root of a number
+// echo sqrt(144);
+
+// finds max and min respectively
+// echo max(2,10);
+// echo min(10 , 2);
+
+// rounds numbers up normally
+// echo round(3.7);
+
+// the below comand rounds the number up even if the decimal is small
+// echo ceil(3.3 );
+
+// rounds the number down even with a big decimal
+// echo floor(3.8 );
+
+<br> 
+
+
+
+    
+</body>
+</html>
